b96f9b04b9a965159a712a92703bebfe12da8dfb
tarekfouda <tarekfouda0@gmail.com>
2021-07-11 17:24:09 +0200

    Offers.dart

--- lib/user/screens/languages/LanguagesData.dart
+++ lib/user/screens/languages/LanguagesData.dart
@@ -8,6 +8,9 @@ class LanguagesData{
   setLanguage(BuildContext context,String lang)async{
     langCubit.onUpdateData(lang.toString());
     Utils.changeLanguage(lang.toString(), context);
+    var user = context.read<UserCubit>();
+    user.state.model.lang=lang;
+    user.onUpdateUserData(user.state.model);
   }
 
 
